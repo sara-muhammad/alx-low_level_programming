72ac0affabe381ffbef7427d7a3c5e3da37781e2
sara-muhammad <saraelakaad@gmail.com>
2023-08-10 15:20:11 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Aprogram that use printf function
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}

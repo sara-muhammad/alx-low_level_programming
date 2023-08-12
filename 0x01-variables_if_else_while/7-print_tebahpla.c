6e78000f7edb0d8d2261fb3e3a2c72abe2ec3034
sara-muhammad <saraelakaad@gmail.com>
2023-08-11 18:59:34 -0700

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry pointer
+ *
+ * Description: A c programm prints alphabet in lower case
+ *
+ * Return: 0(Success)
+*/
+
+
+int main(void)
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfdecba";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+	return (0);
+}

59717ab3e1a29bda0be0930d3577a962771da1a8
sara-muhammad <saraelakaad@gmail.com>
2023-08-11 18:25:42 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry pointer
+ *
+ * Description: A c programm prints alphabet in lower case
+ *
+ * Return: 0(Success)
+*/
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}

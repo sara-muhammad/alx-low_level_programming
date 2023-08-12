8424c42bac5c63f9c6ad5dc5c7bb5ae5b462148a
sara-muhammad <saraelakaad@gmail.com>
2023-08-11 18:45:24 -0700

    m

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,12 +11,11 @@
 
 int main(void)
 {
-	int my_numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(my_numbers[i]);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);

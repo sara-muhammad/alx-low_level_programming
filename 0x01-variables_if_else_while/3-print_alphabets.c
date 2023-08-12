d792c0d5d2846040795792abafc63efbb7f53763
sara-muhammad <saraelakaad@gmail.com>
2023-08-11 17:39:28 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry pointer
+ *
+ * Description: A c programm prints alphabet in lower case
+ *
+ * Return: 0(Success)
+*/
+
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char aup[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+		putchar(aup[i]);
+	}
+	putchar('\n');
+	return (0);
+}

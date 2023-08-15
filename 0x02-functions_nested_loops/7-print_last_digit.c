121cce93eb283526cff01271ee27494d514434a7
sara-muhammad <saraelakaad@gmail.com>
2023-08-15 06:05:46 -0700

    b

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -21,7 +21,7 @@ int print_last_digit(int n)
 	else
 		last_digit = n % 10;
 	if (last_digit < 0)
-		last_digit = -last digit;
+		last_digit = last_digit;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }

70647a33782b86cdbc52eb0b5cbec26bc2a89c27
sara-muhammad <saraelakaad@gmail.com>
2023-08-15 15:59:21 -0700

    b

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,8 +12,20 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
+		printf("%d", 98);
+	}
+
+	else if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+		printf("%d", 98);
+	}
+	else
+	{
+		printf("%d", n);
 	}
 	printf("\n");
 }

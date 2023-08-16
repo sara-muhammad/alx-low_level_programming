477d73391f2e099c29ff976ecd895474d25edb80
sara-muhammad <saraelakaad@gmail.com>
2023-08-16 10:14:56 -0700

    b

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: programm computes fibonacci numbers
+ *
+ * Return: 0(success)
+*/
+
+
+int main(void)
+{
+	int n;
+	int fib[];
+	int sum;
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (n = 2; n <= 49; n++)
+	{
+		sum = fib[n - 2] + fib[n - 1];
+	}
+	printf("%d\n", sum);
+	return (0);
+}

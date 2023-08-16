993948622749d0fdf52faea9ed1a49d20693d38d
sara-muhammad <saraelakaad@gmail.com>
2023-08-16 06:03:48 -0700

    b

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,9 +14,9 @@ void print_times_table(int n)
 	{
 		for (x = 0; x <= n; x++)
 		{
+			_putchae(48);
 			for (y = 1; y <= n; y++)
 			{
-				_putchar(48);
 				_putchar(',');
 				_putchar(' ');
 				prod = x * y;
@@ -25,12 +25,16 @@ void print_times_table(int n)
 				if (prod < 100)
 				{
 					_putchar(' ');
-					_putchar((prod / 10) + 48);
 				}
 				if (prod > 100)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) + 48);
+				}
+				else if (prod >= 10 && prod <= 99)
+				{
+					_putchar ((prod / 10) + 48);
+
 				}
 				_putchar((prod % 10) + 48);
 

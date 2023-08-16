9938cde23d8075d6e68b628e000db1088065459c
sara-muhammad <saraelakaad@gmail.com>
2023-08-16 13:07:55 -0700

    b

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,10 +26,15 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 				}
-				if (prod >= 100)
+				if (prod > 100)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) + 48);
+				}
+				if (prod == 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar(48);
 				}
 				else if (prod >= 10 && prod <= 99)
 				{

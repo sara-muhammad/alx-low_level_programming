f07c18ef09554f4e36b1bb8589fe833aec33bb44
sara-muhammad <saraelakaad@gmail.com>
2023-08-17 06:04:23 -0700

    commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * positive_or_negative - function to check number sign
+ *
+ * @i : the number will bet checked
+ */
+
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+	return;
+
+}

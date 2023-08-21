6fd16b3a8c6c98259116e91f812f2b4f433f0926
sara-muhammad <saraelakaad@gmail.com>
2023-08-21 14:20:20 -0700

    b

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ *
+ * @s: function input.
+ */
+void rev_string(char *s)
+{
+	int i,j;
+	char temp;
+
+	for (j = 0; s[j] != '\0'; j++)
+		;
+	for (i = 0; i < j / 2; i++ )
+	{
+		temp = s[i];
+		s[i] = s[j - 1 - i];
+		s[j - 1 - i] = temp;
+	}
+
+}

e3f370b462ef76addaa0eb30930379f706b6e386
sara-muhammad <saraelakaad@gmail.com>
2023-08-21 13:18:25 -0700

    b

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *
+ * @s: function input.
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

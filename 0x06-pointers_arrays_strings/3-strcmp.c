333388d8e17ad6b3fe86252dff7069e5efbf4e9a
sara-muhammad <saraelakaad@gmail.com>
2023-08-24 14:42:44 -0700

    m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,21 +12,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
-
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-	if (s1 != s2)
-	{
-		flag = 1;
+		s1++;
+		s2++;
 	}
-	s1++;
-	s2++;
-	}
-	if (*s1 != '\0' || *s2 != '\0')
-	return (1);
-	if (flag == 0)
-	return (0);
-	else
-	return (1);
+	return (*s1 - *s2);
 }

8e8156d4a0bd151c7fd5a987a954d90585557cc7
sara-muhammad <saraelakaad@gmail.com>
2023-08-29 20:28:07 -0700

    m

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - check the code
+ *
+ * @s: function input
+ *
+ * @to: function input
+ */
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

757efaad3869336c3b025e0b328552892309cddd
sara-muhammad <saraelakaad@gmail.com>
2023-08-30 15:34:45 -0700

    m

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check the code
+ *
+ * @s: function input
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i += _strlen_recursion(s+1)+1;
+	}
+	return(i);
+}

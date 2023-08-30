4fcdc964d0c343e3c4f764bd265c78c704750c95
sara-muhammad <saraelakaad@gmail.com>
2023-08-30 15:40:41 -0700

    m

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,6 +5,8 @@
  * _strlen_recursion - check the code
  *
  * @s: function input
+ *
+ * Return : Always 0
  */
 
 
@@ -14,7 +16,7 @@ int _strlen_recursion(char *s)
 
 	if (*s)
 	{
-		i += _strlen_recursion(s+1)+1;
+		i += _strlen_recursion(s + 1) + 1;
 	}
-	return(i);
+	return (i);
 }

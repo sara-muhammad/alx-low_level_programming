e69ba48f1a53ce13c58bb5716803f683920dd680
sara-muhammad <saraelakaad@gmail.com>
2023-09-05 14:41:19 -0700

    m

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,8 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *)malloc(size * sizeof(char));
 
-	if (size == 0)
-		return (NULL);
+	if (size == 0 || ptr = 0)
+		return (0);
 
 	while (size--)
 	{

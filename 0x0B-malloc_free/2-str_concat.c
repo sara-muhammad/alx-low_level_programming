4426954b8e43d23b66fe8db87658f56c16deb9ea
sara-muhammad <saraelakaad@gmail.com>
2023-09-05 14:04:57 -0700

    m

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - check the code for ALX School students.
+ * @s1: first array
+ * @s2: second array
+ * Return: Always 0.
+ */
+
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, i;
+	char *ptr;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	for (len1 = 1; s1[len1] != '\0'; len1++)
+	;
+	for (len2 = 1; s2[len2] != '\0'; len2++)
+	;
+	ptr = (char *) malloc((len1 + len2) * sizeof(*s1) + 1);
+	if (ptr == NULL)
+		return (0);
+	for (i = 0; i <= (len1 + len2); i++)
+	{
+		if (i < len1)
+			ptr[i] = s1[i];
+		else
+			ptr[i] = s2[i - len1];
+	}
+
+	return (ptr);
+}

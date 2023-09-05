65484e68a7c17d99d2d2b946deae95ec4e9bd402
sara-muhammad <saraelakaad@gmail.com>
2023-09-05 14:16:54 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
 	for (len1 = 1; s1[len1] != '\0'; len1++)
 	;
 	for (len2 = 1; s2[len2] != '\0'; len2++)
@@ -31,6 +33,7 @@ char *str_concat(char *s1, char *s2)
 		else
 			ptr[i] = s2[i - len1];
 	}
+	ptr[i] = '\0';
 
 	return (ptr);
 }

4d4a84e8892d616f41cb5a4debf9aa82e89f01b1
sara-muhammad <saraelakaad@gmail.com>
2023-09-05 19:08:16 -0700

    a

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * alloc_grid - check the code for ALX School students.
+ * @width: width of grid
+ * @height: height of grid
+ * Return: Always 0.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ptr = malloc(height * sizeof(*ptr));
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(width * sizeof(**ptr));
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+	return (ptr);
+
+}

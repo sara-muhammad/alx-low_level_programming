6b20a8a06df7f782179e98707e1d7ab911591345
sara-muhammad <saraelakaad@gmail.com>
2023-09-11 12:16:40 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,12 +18,12 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t
+typedef struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
-}
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif

24e625495b4cb62b4301cf674c61b6e95a5aa729
sara-muhammad <saraelakaad@gmai.com>
2023-09-23 00:55:01 -0700

    a

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
+char **strtow(char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);

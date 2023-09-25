a66a048c8ea12fba8824dc00ca8edd2e276e93d2
sara-muhammad <saraelakaad@gmai.com>
2023-09-25 09:16:51 -0700

    a

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *node = h;
+	const listint_t *cursor = h;
 	size_t count = 0;
 
-	while (node != NULL)
+	while (cursor != NULL)
 	{
-		printf("%d\n", node->n);
+		printf("%d\n", cursor->n);
 		count += 1;
-		node = node->next;
+		cursor = cursor->next;
 	}
 	return (count);
 }

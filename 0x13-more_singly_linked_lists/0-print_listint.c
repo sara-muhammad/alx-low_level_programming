64244baf58d9fa4ef768f7683b3b8e0ce1913b3f
sara-muhammad <saraelakaad@gmai.com>
2023-09-25 11:25:33 -0700

    a

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t count= 0;
 
 	while (h)
 	{
-		nodes++;
+		count++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (nodes);
+	return (count);
 }
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head - pointer to head of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *cursor;
+	
+	while (head)
+	{
+		cursor = head->next;
+		free(head);
+		head = cursor;
+	}
+	
+
+}

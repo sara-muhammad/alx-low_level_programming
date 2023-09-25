7c75b3096e6aae23f3b27eec589099c319f42b91
sara-muhammad <saraelakaad@gmai.com>
2023-09-25 11:29:50 -0700

    a

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,19 +2,18 @@
 
 /**
  * free_listint - free list
- * @head - pointer to head of the list
+ * @head: pointer to head of the list
  */
 
 void free_listint(listint_t *head)
 {
 	listint_t *cursor;
-	
+
 	while (head)
 	{
 		cursor = head->next;
 		free(head);
 		head = cursor;
 	}
-	
 
 }

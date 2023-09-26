b7360796affd843afbf0d894ea5caf94b2a4134e
sara-muhammad <saraelakaad@gmai.com>
2023-09-26 13:57:11 -0700

    a

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int sum_listint(listint_t *head);
 int pop_listint(listint_t **head);

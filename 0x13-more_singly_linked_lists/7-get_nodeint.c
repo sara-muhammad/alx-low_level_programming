3f45c7d54906d4ffe51bd7eb3f01dc2c52eb188a
sara-muhammad <saraelakaad@gmai.com>
2023-09-26 02:30:10 -0700

    a

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,9 +9,9 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int node;
+	unsigned int i;
 
-	for (node = 0; node < index; node++)
+	for (i= 0; i < index; i++)
 	{
 		if (head == NULL)
 			return (NULL);

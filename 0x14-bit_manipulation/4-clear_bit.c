2ddc3e0d80eb58751896c2029cae5684aec41371
sara-muhammad <saraelakaad@gmai.com>
2023-09-28 16:04:04 -0700

    edit betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,13 +10,12 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
-	
+
 	if (index > 63)
 		return (-1);
 	m = 1 << index;
 
 	if (*n & m)
 		*n ^= m;
- 
 	return (1);
 }

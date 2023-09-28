d220b2d5891459398ff308bf2185dc478f908ea6
sara-muhammad <saraelakaad@gmai.com>
2023-09-28 16:24:54 -0700

    task5

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);

64309827fc8c43b2b0a625904cce2ea0d5c6bcd0
sara-muhammad <saraelakaad@gmai.com>
2023-10-02 14:50:03 -0700

    a

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,10 +5,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <elf.h>
 
 
+
+
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 
 
